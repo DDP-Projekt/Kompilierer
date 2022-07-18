40b4c4545593829788efeb0c07bd69aca6d36c76
NotLe0n <notle0n75@gmail.com>
2022-07-18 13:16:18 +0200

    rng stdlib functions
    implementation missing

---
+++ lib/stdlib/source/random.c
@@ -0,0 +1,14 @@
+#include "ddptypes.h"
+#include "math.h"
+#include "time.h"
+
+ddpfloat ddpextern_ZufallFloat(ddpfloat a, ddpfloat b) {
+}
+
+ddpint ddpextern_ZufallInt(ddpint a, ddpint b) {
+
+}
+
+ddpbool ddpextern_ZufallBool(ddpfloat p) {
+	
+}
