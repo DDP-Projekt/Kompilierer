12f9e98d6575afc106a2f3df5d0fc94dec57aa3f
NotLe0n <notle0n75@gmail.com>
2022-08-22 16:52:28 +0200

    added Eingabe tests (still failing)

--- tests/kddp_test.go
+++ tests/kddp_test.go
@@ -44,28 +44,50 @@ func runTests(t *testing.T, ignoreFile string, path string, d fs.DirEntry, err e
 
 	t.Run(d.Name(), func(t *testing.T) {
 		t.Parallel()
+
+		// read expected.txt
 		expected, err := os.ReadFile(filepath.Join(path, "expected.txt"))
 		if err != nil {
 			t.Errorf("Could not read expected output: %s", err.Error())
 			return
 		}
 
+		// get ddp file path
 		filename := filepath.Join(path, filepath.Base(path)) + ".ddp"
+
+		// build dpp file
 		cmd := exec.Command("../build/kddp", "build", changeExtension(filename, ".ddp"), "-o", changeExtension(filename, ".exe"), "--verbose")
+		// get build output
 		if out, err := cmd.CombinedOutput(); err != nil {
+			// error if failed
 			t.Errorf("compilation failed: %s\ncompiler output: %s", err, string(out))
 			return
 		} else {
+			// remove exe if successful
 			defer os.Remove(changeExtension(filename, ".exe"))
 		}
 
+		// run ddp executeable
 		cmd = exec.Command(changeExtension(filename, ".exe"))
+
+		// read input
+		input, err := os.Open(filepath.Join(path, "input.txt"))
+		// if input.txt exists
+		if err == nil {
+			// replace stdin with input.txt
+			cmd.Stdin = input
+		}
+
+		// get output
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Errorf("\nerror getting combined output: %s\noutput:\n%s", err, out)
 			return
 		}
 
+		input.Close() // close input file
+
+		// error if 'out' was not the expected output
 		if out, expected := string(out), string(expected); out != expected {
 			t.Errorf("Test did not yield the expected output\n"+
 				"\x1b[1;32mExpected:\x1b[0m\n%s\n"+
