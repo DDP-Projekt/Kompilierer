a05b73f150209b11f7c90f697cb637ebdcca9400
Hendrik Ziegler <hendrikziegler@outlook.de>
2022-09-26 20:30:52 +0200

    changed the Relocation Mode of llvm to fix a issue on linux

--- internal/linker/link.go
+++ internal/linker/link.go
@@ -100,7 +100,7 @@ func LinkDDPFiles(options Options) error {
 
 	libdir := filepath.Join(scanner.DDPPATH, "lib")
 
-	args := append(make([]string, 0), "-o", options.OutputFile, "-O2", "-Wl,-v", "-L"+libdir)
+	args := append(make([]string, 0), "-o", options.OutputFile, "-O2", "-L"+libdir)
 
 	// add all librarie-search-paths
 	for k := range link_objects {
@@ -125,9 +125,7 @@ func LinkDDPFiles(options Options) error {
 
 	cmd := exec.Command("gcc", args...)
 	options.Log("%s", cmd.String())
-	out, err := cmd.CombinedOutput()
-	options.Log("%s", out)
-	return err
+	return cmd.Run()
 }
 
 // helper for invokeGCC
