f487dfddae45c18be01f0fea3a90b43ba717ffef
bafto <hendrikziegler@outlook.de>
2023-03-03 10:44:11 +0100

    removed the tool/gen/ directory as we don't need it anymore. The compilier does its job now

--- tool/gen/templates/tmpl.ddptypes.h
+++
@@ -1,9 +0,0 @@
-{{range .}}
-typedef struct {
-	{{ .E }}* arr; // the element array
-	ddpint len; // the length of the array
-	ddpint cap; // the capacity of the array
-} {{ .T }};
-
-void _ddp_free_{{ .T }}({{ .T }}* list);
-{{end}}
