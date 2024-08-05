8bca1892a05d9354802214b89f1b40feccab26e6
Hendrik Ziegler <hendrikziegler@outlook.de>
2024-08-05 10:13:59 +0200

    fixes to utf8-handling + big performance improvements for (some) lists/string Duden functions

--- lib/runtime/source/utf8/utf8.c
+++ lib/runtime/source/utf8/utf8.c
@@ -4,6 +4,7 @@
 */
 #include "utf8/utf8.h"
 #include <string.h>
+#include <uchar.h>
 
 // check if the first unicode character in c is a single-byte character
 bool utf8_is_single_byte(char *c) {
@@ -106,73 +107,47 @@ size_t utf8_num_bytes(char *s) {
 	return num_bytes;
 }
 
+// returns the number of bytes needed to encode this character in utf8
+size_t utf8_num_bytes_char(uint32_t c) {
+	if ((int32_t)c < 0) {
+		return -1;
+	} else if (c <= ((1 << 7) - 1)) {
+		return 1;
+	} else if (c <= ((1 << 11) - 1)) {
+		return 2;
+	} else if (0xD800 <= c && c <= 0xDFFF) {
+		return -1;
+	} else if (c <= ((1 << 16) - 1)) {
+		return 3;
+	} else if (c <= 0x0010FFFF) {
+		return 4;
+	}
+	return -1;
+}
+
+static mbstate_t state;
+
 // decodes the unicode character c into s
 // s must be at least 5 chars long and will be null-terminated by the functions
 // returns the number of bytes in c
 // returns -1 if c is not a valid utf8 character
-int utf8_char_to_string(char *s, int32_t c) {
-	// copied from the Go stdlib (see utf8.go in unicode/utf8)
-	uint32_t ch = (uint32_t)c;
-	int num_bytes = 0;
-	if (ch <= 127) {
-		s[0] = (char)c;
-		num_bytes = 1;
-	} else if (ch <= 2047) {
-		s[0] = 192 | (char)(c >> 6);
-		s[1] = 128 | (((char)c) & 63);
-		num_bytes = 2;
-	} else if (ch > 1114111 || (55296 <= ch && ch <= 57343)) {
-		s[0] = '\0';
-		return -1;
-	} else if (ch <= 65535) {
-		s[0] = 224 | (char)(c >> 12);
-		s[1] = 128 | (((char)c >> 6) & 63);
-		s[2] = 128 | (((char)c) & 63);
-		num_bytes = 3;
-	} else {
-		s[0] = 240 | (char)(c >> 18);
-		s[1] = 128 | (char)(c >> 12);
-		s[2] = 128 | (((char)c >> 6) & 63);
-		s[3] = 128 | (((char)c) & 63);
+size_t utf8_char_to_string(char *s, int32_t c) {
+	size_t num_bytes = c32rtomb(s, c, &state);
+	if (num_bytes != (size_t)-1) {
+		s[num_bytes] = '\0';
 	}
-	s[num_bytes] = '\0';
 	return num_bytes;
 }
 
-// copied from https://rosettacode.org/wiki/UTF-8_encode_and_decode#C
-
-typedef struct {
-	unsigned char mask; // char data will be bitwise AND with this
-	unsigned char lead; // start bytes of current char in utf-8 encoded character
-	uint32_t beg;		// beginning of codepoint range
-	uint32_t end;		// end of codepoint range
-	int bits_stored;	// the number of bits from the codepint that fits in char
-} utf_t;
-
-const utf_t *utf[] = {
-	/*             mask        lead        beg      end       bits */
-	[0] = &(utf_t){0x3F, 0x80, 0, 0, 6},
-	[1] = &(utf_t){0x7F, 0x00, 0000, 0177, 7},
-	[2] = &(utf_t){0x1F, 0xC0, 0200, 03777, 5},
-	[3] = &(utf_t){0x0F, 0xE0, 04000, 0177777, 4},
-	[4] = &(utf_t){0x07, 0xF0, 0200000, 04177777, 3},
-	&(utf_t){0},
-};
-
-// decode the first codepoint in str
+// decode the first codepoint in str into out
 // str must be null-terminated
-int32_t utf8_string_to_char(char *str) {
+// returns the number of bytes encoded into out or -1 if str was invalid utf8
+size_t utf8_string_to_char(char *str, uint32_t *out) {
 	if (str == NULL) {
 		return -1;
 	}
-	int num_bytes = utf8_num_bytes(str);
-	int shift = utf[0]->bits_stored * (num_bytes - 1);
-	int32_t codep = (*str++ & utf[num_bytes]->mask) << shift;
 
-	for (int i = 1; i < num_bytes; ++i, ++str) {
-		shift -= utf[0]->bits_stored;
-		codep |= ((char)*str & utf[0]->mask) << shift;
-	}
-
-	return codep;
+	size_t n = utf8_num_bytes(str);
+	mbrtoc32(out, str, n, &state);
+	return n;
 }
--- lib/stdlib/source/lists.c
+++ lib/stdlib/source/lists.c
@@ -1,8 +1,7 @@
 #include "ddpmemory.h"
 #include "ddptypes.h"
-#include <math.h>
+#include "utf8/utf8.h"
 #include <string.h>
-
 
 typedef struct {
 	void *arr;
@@ -45,8 +44,12 @@ void efficient_list_append_char(ddpcharlistref list, ddpcharref elem, ddpint ele
 
 void efficient_list_append_string(ddpstringlistref list, ddpstringref elem, ddpint elem_size) {
 	efficient_list_append((generic_list_ref)list, (generic_ref)elem, elem_size);
-	elem->cap = 0;
-	elem->str = NULL;
+	*elem = DDP_EMPTY_STRING;
+}
+
+void efficient_list_append_any(ddpanylistref list, ddpanyref elem, ddpint elem_size) {
+	efficient_list_append((generic_list_ref)list, (generic_ref)elem, elem_size);
+	*elem = DDP_EMPTY_ANY;
 }
 
 static void efficient_list_prepend(generic_list_ref list, generic_ref elem, ddpint elem_size) {
@@ -74,11 +77,15 @@ void efficient_list_prepend_char(ddpcharlistref list, ddpcharref elem, ddpint el
 
 void efficient_list_prepend_string(ddpstringlistref list, ddpstringref elem, ddpint elem_size) {
 	efficient_list_prepend((generic_list_ref)list, (generic_ref)elem, elem_size);
-	elem->cap = 0;
-	elem->str = NULL;
+	*elem = DDP_EMPTY_STRING;
 }
 
-#define CLAMP(index, len) ((index) < 0 ? 0 : ((index) >= (len) ? (len)-1 : (index)))
+void efficient_list_prepend_any(ddpanylistref list, ddpanyref elem, ddpint elem_size) {
+	efficient_list_prepend((generic_list_ref)list, (generic_ref)elem, elem_size);
+	*elem = DDP_EMPTY_ANY;
+}
+
+#define CLAMP(index, len) ((index) < 0 ? 0 : ((index) >= (len) ? (len) - 1 : (index)))
 
 // the range is inclusive [start, end]
 // the indices are 0-based (like in C, not like in DDP)
@@ -88,7 +95,7 @@ static void efficient_list_delete_range(generic_list_ref list, ddpint start, ddp
 	}
 
 	if (start > end) {
-		ddp_runtime_error(1, "start index ist größer als end index ("DDP_INT_FMT", "DDP_INT_FMT")", start, end);
+		ddp_runtime_error(1, "start index ist größer als end index (" DDP_INT_FMT ", " DDP_INT_FMT ")", start, end);
 	}
 	start = CLAMP(start, list->len);
 	end = CLAMP(end, list->len);
@@ -121,7 +128,7 @@ void efficient_list_delete_range_string(ddpstringlistref list, ddpint start, ddp
 	}
 
 	if (start > end) {
-		ddp_runtime_error(1, "start index ist größer als end index ("DDP_INT_FMT", "DDP_INT_FMT")", start, end);
+		ddp_runtime_error(1, "start index ist größer als end index (" DDP_INT_FMT ", " DDP_INT_FMT ")", start, end);
 	}
 
 	// free the old strings before shallow-copying them
@@ -133,10 +140,29 @@ void efficient_list_delete_range_string(ddpstringlistref list, ddpint start, ddp
 	efficient_list_delete_range((generic_list_ref)list, start, end, elem_size);
 }
 
+void efficient_list_delete_range_any(ddpanylistref list, ddpint start, ddpint end, ddpint elem_size) {
+	// duplicate logic, but better safe than sorry
+	if (list->len <= 0) {
+		return;
+	}
+
+	if (start > end) {
+		ddp_runtime_error(1, "start index ist größer als end index (" DDP_INT_FMT ", " DDP_INT_FMT ")", start, end);
+	}
+
+	// free the old strings before shallow-copying them
+	ddpint free_end = CLAMP(end, list->len);
+	for (ddpint i = CLAMP(start, list->len); i <= free_end; i++) {
+		ddp_free_any(&list->arr[i]);
+	}
+
+	efficient_list_delete_range((generic_list_ref)list, start, end, elem_size);
+}
+
 // the index is 0-based (like in C, not like in DDP)
 void efficient_list_insert(generic_list_ref list, ddpint index, generic_ref elem, ddpint elem_size) {
 	if (index < 0 || index > list->len) {
-		ddp_runtime_error(1, "Index außerhalb der Listen Länge (Index war "DDP_INT_FMT", Listen Länge war "DDP_INT_FMT")", index, list->len);
+		ddp_runtime_error(1, "Index außerhalb der Listen Länge (Index war " DDP_INT_FMT ", Listen Länge war " DDP_INT_FMT ")", index, list->len);
 	}
 
 	grow_if_needed(list, elem_size);
@@ -163,14 +189,18 @@ void efficient_list_insert_char(ddpcharlistref list, ddpint index, ddpcharref el
 
 void efficient_list_insert_string(ddpstringlistref list, ddpint index, ddpstringref elem, ddpint elem_size) {
 	efficient_list_insert((generic_list_ref)list, index, (generic_ref)elem, elem_size);
-	elem->cap = 0;
-	elem->str = NULL;
+	*elem = DDP_EMPTY_STRING;
+}
+
+void efficient_list_insert_any(ddpanylistref list, ddpint index, ddpanyref elem, ddpint elem_size) {
+	efficient_list_insert((generic_list_ref)list, index, (generic_ref)elem, elem_size);
+	*elem = DDP_EMPTY_ANY;
 }
 
 // the index is 0-based (like in C, not like in DDP)
 void efficient_list_insert_range(generic_list_ref list, ddpint index, generic_list_ref other, ddpint elem_size) {
 	if (index < 0 || index > list->len) {
-		ddp_runtime_error(1, "Index außerhalb der Listen Länge (Index war "DDP_INT_FMT", Listen Länge war "DDP_INT_FMT")", index, list->len);
+		ddp_runtime_error(1, "Index außerhalb der Listen Länge (Index war " DDP_INT_FMT ", Listen Länge war " DDP_INT_FMT ")", index, list->len);
 	}
 
 	ddpint new_len = list->len + other->len;
@@ -206,4 +236,29 @@ void efficient_list_insert_range_string(ddpstringlistref list, ddpint index, ddp
 	for (ddpint i = 0; i < other->len; i++) {
 		other->arr[i] = DDP_EMPTY_STRING;
 	}
+}
+
+void efficient_list_insert_range_any(ddpanylistref list, ddpint index, ddpanylistref other, ddpint elem_size) {
+	efficient_list_insert_range((generic_list_ref)list, index, (generic_list_ref)other, elem_size);
+	for (ddpint i = 0; i < other->len; i++) {
+		other->arr[i] = DDP_EMPTY_ANY;
+	}
+}
+
+void Aneinandergehaengt_Buchstabe_Ref(ddpstring *ret, ddpcharlistref liste) {
+	size_t num_bytes = 0;
+	ddpchar *end = &liste->arr[liste->len];
+	for (ddpchar *it = liste->arr; it != end; it++) {
+		num_bytes += utf8_num_bytes_char(*it);
+	}
+
+	ret->str = DDP_ALLOCATE(char, num_bytes + 1);
+	ret->cap = num_bytes + 1;
+
+	char *str_it = ret->str;
+	for (ddpchar *it = liste->arr; it != end; it++) {
+		str_it += utf8_char_to_string(str_it, *it);
+	}
+
+	ret->str[num_bytes] = '\0';
 }
