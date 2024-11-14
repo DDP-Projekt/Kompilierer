a92e7eb7f97aa33e00b9befb50b42fc0732d3905
NotLe0n <notle0n75@gmail.com>
2024-11-14 16:28:57 +0100

    Archiv_Aus_Ordner and ddp error handling

--- lib/stdlib/source/DDP/compression.c
+++ lib/stdlib/source/DDP/compression.c
@@ -1,5 +1,6 @@
 #include "DDP/ddptypes.h"
 #include "DDP/ddpos.h"
+#include "DDP/error.h"
 #include <archive.h>
 #include <archive_entry.h>
 #include <stdio.h>
@@ -9,98 +10,188 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
+
+#define ARCHIVE_FILTER(packed) (packed & 0xF)
+#define ARCHIVE_FORMAT(packed) ((packed & 0xFFFFF0) >> 4)
+
+// creates an archive at the given path with filetype and compression algorithm packed into the type.
+// see ARCHIVE_FILTER(packed) and ARCHIVE_FORMAT(packed).
+// returns the archive object if successful and NULL (0) if not.
+struct archive* createArchive(const char *path, int type) {
+	// Create a new archive object for writing
+	struct archive *a = archive_write_new();
+	if (!a) {
+		ddp_error("Failed to create archive", false);
+		return NULL;
+	}
+
+	// Extract filter bits and set the compression filter
+	if (archive_write_add_filter(a, ARCHIVE_FILTER(type)) != ARCHIVE_OK) {
+		ddp_error("Failed to set compression algorithm: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_write_free(a);
+		return NULL;
+	}
+
+	// Extract format bits and set the file format
+	if (archive_write_set_format(a, ARCHIVE_FORMAT(type)) != ARCHIVE_OK) {
+		ddp_error("Failed to set archive format: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_write_free(a);
+		return NULL;
+	}
+
+	if (archive_write_open_filename(a, path) != ARCHIVE_OK) {
+		ddp_error("Failed to open output file: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_write_free(a);
+		return NULL;
+	}
+
+	return a;
+}
+
+// create an entry in the given archive with the content of the file at the given path.
+// returns true (1) if successful and false (0) if not.
+int createEntry(struct archive *a, const char *path) {
+	printf("%s\n", path);
+	struct stat st;
+	if (stat(path, &st) < 0) {
+		ddp_error("Failed to stat input file "DDP_STRING_FMT" :", true, path);
+		archive_write_free(a);
+		return 0;
+	}
+
+	// Create a new entry for the file
+	struct archive_entry *entry = archive_entry_new();
+	archive_entry_set_pathname(entry, path);
+	archive_entry_copy_stat(entry, &st);
+	archive_entry_set_perm(entry, 0644);
+
+	// Write the header for the entry
+	if (archive_write_header(a, entry) != ARCHIVE_OK) {
+		ddp_error("Failed to write header: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_entry_free(entry);
+		archive_write_free(a);
+		return 0;
+	}
+
+	// Open the input file
+	FILE *file = fopen(path, "rb");
+	if (!file) {
+		ddp_error("Failed to open input file: "DDP_STRING_FMT, false, path);
+		archive_write_free(a);
+		return 0;
+	}
+
+	char buffer[8192];
+	size_t bytes_read;
+	// Write the file data to the archive entry
+	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+		if (archive_write_data(a, buffer, bytes_read) != bytes_read) {
+			ddp_error("Failed to write data: "DDP_STRING_FMT, false, archive_error_string(a));
+			fclose(file);
+			archive_entry_free(entry);
+			archive_write_free(a);
+			return 0;
+		}
+	}
+
+	// Clean up
+	fclose(file);
+	archive_entry_free(entry);
+	return 1;
+}
+
+// walks a directory and creates a new entry for each file
+void compressDir(struct archive *a, char *path) {
+	DIR* dir;
+	struct dirent *ent;
+	struct stat states;
+
+	dir = opendir(path);
+	if (!dir) {
+		ddp_error("Couldn't open directory "DDP_STRING_FMT": ", true, path);
+		return;
+	}
+
+	while ((ent=readdir(dir)) != NULL) {
+		if (!strcmp(".", ent->d_name) || !strcmp("..", ent->d_name)) {
+			continue;
+		}
+
+		char localname[255];
+		strcpy(localname, path);
+		strcat(localname, "/");
+		strcat(localname, ent->d_name);
+
+		if (stat(localname, &states) < 0) {
+			ddp_error("Failed to stat input file "DDP_STRING_FMT": ", true, ent->d_name);
+			return;
+		}
+		
+		if (S_ISDIR(states.st_mode)) {
+			compressDir(a, localname);
+		}
+		else {
+			if (!createEntry(a, localname)) {
+				closedir(dir);
+				return;
+			}
+		}
+	}
+
+    closedir(dir);
+}
 
 // typ: | 16Bit format | 4 Bit filter | 
 void Archiv_Aus_Datei(ddpint typ, ddpstring *dateiPfad, ddpstring *arPfad) {
-	printf("Adding file %s to archive at path %s\n", dateiPfad->str, arPfad->str);
+	DDP_MIGHT_ERROR;
+	if (dateiPfad == NULL || arPfad == NULL) {
+		ddp_error("Invalid path", false);
+		return;
+	}
 
-    // Create a new archive object for writing
-    struct archive *a = archive_write_new();
-    if (!a) {
-        fprintf(stderr, "Failed to create archive\n");
-        return;
-    }
+	struct archive *a = createArchive(arPfad->str, (int)typ);
+	if (!a) {
+		return;
+	}
 
-    // Extract filter bits and set the compression filter
-    if (archive_write_add_filter(a, typ & 0xF) != ARCHIVE_OK) {
-        fprintf(stderr, "Failed to set compression algorithm: %s\n", archive_error_string(a));
-        archive_write_free(a);
-        return;
-    }
+	if (!createEntry(a, dateiPfad->str)) {
+		return;
+	}
 
-    // Extract format bits and set the file format
-    if (archive_write_set_format(a, (typ & 0xFFFFF0) >> 4) != ARCHIVE_OK) {
-        fprintf(stderr, "Failed to set archive format: %s\n", archive_error_string(a));
-        archive_write_free(a);
-        return;
-    }
+	// Close the archive
+	if (archive_write_close(a) != ARCHIVE_OK) {
+		ddp_error("Failed to close archive: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_write_free(a);
+		return;
+	}
 
-    if (archive_write_open_filename(a, arPfad->str) != ARCHIVE_OK) {
-        fprintf(stderr, "Failed to open output file: %s\n", archive_error_string(a));
-        archive_write_free(a);
-        return;
-    }
-
-	struct stat st;
-	if (stat(dateiPfad->str, &st) < 0) {
-        fprintf(stderr, "Failed to stat input file: %s\n", strerror(errno));
-        archive_write_free(a);
-        return;
-    }
-
-    // Create a new entry for the file
-    struct archive_entry *entry = archive_entry_new();
-    archive_entry_set_pathname(entry, dateiPfad->str);
-    archive_entry_copy_stat(entry, &st);
-	archive_entry_set_perm(entry, 0644);
-
-    // Write the header for the entry
-    if (archive_write_header(a, entry) != ARCHIVE_OK) {
-        fprintf(stderr, "Failed to write header: %s\n", archive_error_string(a));
-        archive_entry_free(entry);
-        archive_write_free(a);
-        return;
-    }
-
-    // Open the input file
-    FILE *file = fopen(dateiPfad->str, "rb");
-    if (!file) {
-        fprintf(stderr, "Failed to open input file: %s\n", dateiPfad->str);
-        archive_write_free(a);
-        return;
-    }
-
-    char buffer[8192];
-    size_t bytes_read;
-    // Write the file data to the archive entry
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        if (archive_write_data(a, buffer, bytes_read) != bytes_read) {
-            fprintf(stderr, "Failed to write data: %s\n", archive_error_string(a));
-            fclose(file);
-            archive_entry_free(entry);
-            archive_write_free(a);
-            return;
-        }
-    }
-
-    // Clean up
-    fclose(file);
-    archive_entry_free(entry);
-
-    // Close the archive
-    if (archive_write_close(a) != ARCHIVE_OK) {
-        fprintf(stderr, "Failed to close archive: %s\n", archive_error_string(a));
-        archive_write_free(a);
-        return;
-    }
-
-    // Free the archive object
-    archive_write_free(a);
+	// Free the archive object
+	archive_write_free(a);
 }
 
 void Archiv_Aus_Ordner(ddpint typ, ddpstring *ordnerPfad, ddpstring *arPfad) {
-	
+	DDP_MIGHT_ERROR;
+	if (ordnerPfad == NULL || arPfad == NULL) {
+		ddp_error("Invalid path", false);
+		return;
+	}
+
+	struct archive *a = createArchive(arPfad->str, (int)typ);
+	if (!a) {
+		return;
+	}
+
+	compressDir(a, ordnerPfad->str);
+
+	// Close the archive
+	if (archive_write_close(a) != ARCHIVE_OK) {
+		ddp_error("Failed to close archive: "DDP_STRING_FMT, false, archive_error_string(a));
+		archive_write_free(a);
+		return;
+	}
+
+	// Free the archive object
+	archive_write_free(a);
 }
 
 void Archiv_Entpacken_Ordner(ddpstring *arPfad, ddpstring *ordnerPfad) {
