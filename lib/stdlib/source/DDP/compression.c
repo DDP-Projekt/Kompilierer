27ccbd8ae9d9c1e68b4e43945cd2758a8140bed8
NotLe0n <notle0n75@gmail.com>
2024-11-17 13:34:40 +0100

    large compression lib changes

--- lib/stdlib/source/DDP/compression.c
+++ lib/stdlib/source/DDP/compression.c
@@ -1,8 +1,7 @@
 #include "DDP/ddptypes.h"
 #include "DDP/ddpos.h"
 #include "DDP/error.h"
-#include <archive.h>
-#include <archive_entry.h>
+#include "DDP/ddptypes.h"
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -13,8 +12,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define LIBARCHIVE_STATIC
+
+#include <archive.h>
+#include <archive_entry.h>
+
 #define ARCHIVE_FILTER(packed) (packed & 0xF)
 #define ARCHIVE_FORMAT(packed) ((packed & 0xFFFFF0) >> 4)
+
+#define MAX_PATH 260
+#define READ_BUFFER 8192
 
 // creates an archive at the given path with filetype and compression algorithm packed into the type.
 // see ARCHIVE_FILTER(packed) and ARCHIVE_FORMAT(packed).
@@ -26,6 +33,8 @@ struct archive* createArchive(const char *path, int type) {
 		ddp_error("Failed to create archive", false);
 		return NULL;
 	}
+
+	//printf("Filter: %d, Format: 0x%x\n",  ARCHIVE_FILTER(type), ARCHIVE_FORMAT(type));	
 
 	// Extract filter bits and set the compression filter
 	if (archive_write_add_filter(a, ARCHIVE_FILTER(type)) != ARCHIVE_OK) {
@@ -62,56 +71,27 @@ struct archive* createArchive(const char *path, int type) {
 // create an entry in the given archive with the content of the file at the given path.
 // returns true (1) if successful and false (0) if not.
 int createEntry(struct archive *a, const char *path) {
-	struct stat st;
-	if (stat(path, &st) < 0) {
-		ddp_error("Failed to stat input file "DDP_STRING_FMT" :", true, path);
-		archive_write_free(a);
-		return 0;
-	}
+	struct archive *disk = archive_read_disk_new();
+	archive_read_disk_set_standard_lookup(disk);
 
-	// Create a new entry for the file
 	struct archive_entry *entry = archive_entry_new();
-	if (!entry) {
-		ddp_error("Failed to create entry", false);
-		archive_write_free(a);
-		return 0;
-	}
-	
-	archive_entry_set_pathname(entry, path);
-	archive_entry_copy_stat(entry, &st);
-	archive_entry_set_perm(entry, 0644);
-
-	// Write the header for the entry
-	if (archive_write_header(a, entry) != ARCHIVE_OK) {
-		ddp_error("Failed to write header: "DDP_STRING_FMT, false, archive_error_string(a));
-		archive_entry_free(entry);
-		archive_write_free(a);
+	int fd = open(path, O_RDONLY);
+	if (fd < 0) {
 		return 0;
 	}
 
-	// Open the input file
-	FILE *file = fopen(path, "rb");
-	if (!file) {
-		ddp_error("Failed to open input file: "DDP_STRING_FMT, false, path);
-		archive_write_free(a);
-		return 0;
-	}
+	archive_entry_copy_pathname(entry, path);
+	archive_read_disk_entry_from_file(disk, entry, fd, NULL);
+	archive_write_header(a, entry);
 
-	char buffer[8192];
+	char buff[READ_BUFFER];
 	size_t bytes_read;
-	// Write the file data to the archive entry
-	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-		if (archive_write_data(a, buffer, bytes_read) != bytes_read) {
-			ddp_error("Failed to write data: "DDP_STRING_FMT, false, archive_error_string(a));
-			fclose(file);
-			archive_entry_free(entry);
-			archive_write_free(a);
-			return 0;
-		}
+	while ((bytes_read = read(fd, buff, sizeof(buff))) > 0) {
+		archive_write_data(a, buff, bytes_read);
 	}
 
-	// Clean up
-	fclose(file);
+	archive_write_finish_entry(a);
+	archive_read_free(disk);
 	archive_entry_free(entry);
 	return 1;
 }
@@ -133,7 +113,7 @@ void compressDir(struct archive *a, const char *path) {
 			continue;
 		}
 
-		char localname[255];
+		char localname[MAX_PATH];
 		strcpy(localname, path);
 		strcat(localname, "/");
 		strcat(localname, ent->d_name);
@@ -180,16 +160,34 @@ struct archive* openArchive(const char *path) {
 	return a;
 }
 
+void copyData(struct archive *a, struct archive *disk) {
+	int r;
+	const void *buff;
+	size_t size;
+	int64_t offset;
+	while ((r = archive_read_data_block(a, &buff, &size, &offset)) != ARCHIVE_EOF) {
+		if (r != ARCHIVE_OK) {
+			ddp_error("archive_read_data_block()", false, archive_error_string(disk));
+			archive_read_free(a);
+			archive_write_free(disk);
+			return;
+		}
+
+		if (archive_write_data_block(disk, buff, size, offset) != ARCHIVE_OK) {
+			ddp_error("archive_write_data_block()", false, archive_error_string(disk));
+			archive_read_free(a);
+			archive_write_free(disk);
+			return;
+		}
+	}
+}
+
 // typ: | 16Bit format | 4 Bit filter | 
 void Archiv_Aus_Dateien(ddpint typ, ddpstringlist *dateiPfade, ddpstring *arPfad) {
 	DDP_MIGHT_ERROR;
-	if (dateiPfade == NULL || arPfad == NULL) {
+	
+	if (ddp_string_empty(arPfad)) {
 		ddp_error("Invalid path", false);
-		return;
-	}
-
-	if (dateiPfade->len == 0) {
-		ddp_error("No paths given", false);
 		return;
 	}
 
@@ -221,8 +219,8 @@ void Archiv_Aus_Dateien(ddpint typ, ddpstringlist *dateiPfade, ddpstring *arPfad
 
 void Archiv_Aus_Ordner(ddpint typ, ddpstring *ordnerPfad, ddpstring *arPfad) {
 	DDP_MIGHT_ERROR;
-	if (ordnerPfad == NULL || arPfad == NULL) {
-		ddp_error("Invalid path", false);
+	if (ddp_string_empty(ordnerPfad) || ddp_string_empty(arPfad)) {
+		ddp_error("Leerer Pfad", false);
 		return;
 	}
 
@@ -242,12 +240,74 @@ void Archiv_Aus_Ordner(ddpint typ, ddpstring *ordnerPfad, ddpstring *arPfad) {
 	archive_write_free(a);
 }
 
-void Archiv_Entpacken_Ordner(ddpstring *arPfad, ddpstring *ordnerPfad) {
+void Archiv_Entpacken_Dateien_Pfad(ddpstringlist *dateiPfade, ddpstring *arPfad, ddpstring *ordnerPfad) {
+	if (ddp_string_empty(arPfad)) {
+		ddp_error("Leerer Archivpfad gegeben", false);
+		return;
+	}
 
-}
+	struct archive *a = openArchive(arPfad->str);
 
-void Archiv_Entpacken_Dateien(ddpstringlist *dateiPfade, ddpstring *arPfad, ddpstring *pfadZu) {
+	int flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
 
+	struct archive *disk = archive_write_disk_new();
+	archive_write_disk_set_options(disk, flags);
+	archive_read_disk_set_standard_lookup(disk);
+
+	int r;
+	struct archive_entry *entry;
+	while ((r = archive_read_next_header(a, &entry)) != ARCHIVE_EOF) {
+		if (r != ARCHIVE_OK) {
+			ddp_error("Failed to read header: "DDP_STRING_FMT, false, archive_error_string(a));
+			archive_read_free(a);
+			return;
+		}
+
+		// skip all entries not in dateiPfade
+		if (dateiPfade->len != 0) {
+			bool found = false;
+			for (int i = 0; i < dateiPfade->len; i++) {
+				printf("dateiPfade[%d]: %s, entry: %s\n", i, dateiPfade->arr[i].str, archive_entry_pathname(entry));
+				if (!ddp_string_empty(&dateiPfade->arr[i]) && !strcmp(archive_entry_pathname(entry), dateiPfade->arr[i].str)) {
+					printf("found\n");
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+				printf("cont\n");
+				continue;
+			}
+		}
+
+		if (!ddp_string_empty(ordnerPfad)) {
+			printf("path: %s\n", ordnerPfad->str);
+			const char *original_path = archive_entry_pathname(entry);
+			char full_path[MAX_PATH];
+			snprintf(full_path, sizeof(full_path), "%s/%s", ordnerPfad->str, original_path);
+			archive_entry_set_pathname(entry, full_path);
+		}
+
+		if (archive_write_header(disk, entry) != ARCHIVE_OK) {
+			ddp_error("archive_write_header()", false, archive_error_string(disk));
+			archive_read_free(a);
+			archive_write_free(disk);
+			return;
+		}
+
+		copyData(a, disk);
+		
+		if (archive_write_finish_entry(disk) != ARCHIVE_OK) {
+			ddp_error("archive_write_finish_entry()", false, archive_error_string(disk));
+			archive_read_free(a);
+			archive_write_free(disk);
+			return;
+		}
+	}
+
+	archive_read_free(a);
+  	archive_write_free(disk);
 }
 
 void Archiv_Ordner_Hinzufuegen(ddpstring *ordnerPfad, ddpstring *arPfad) {
@@ -268,7 +328,8 @@ void Archiv_Dateien_Entfernen(ddpstringlist *dateiPfade, ddpstring *arPfad) {
 
 ddpint Archiv_Datei_Groesse(ddpstring *dateiPfad, ddpstring *arPfad) {
 	DDP_MIGHT_ERROR;
-	if (!arPfad || !dateiPfad) {
+	
+	if (ddp_string_empty(arPfad) || ddp_string_empty(dateiPfad)) {
 		return -1;
 	}
 	
@@ -297,8 +358,8 @@ ddpint Archiv_Anzahl_Dateien(ddpstring *arPfad) {
 	DDP_MIGHT_ERROR;
 	int count = -1;
 
-	if (!arPfad) {
-		return -1;
+	if (ddp_string_empty(arPfad)) {
+		return 0;
 	}
 	
 	struct archive *a = openArchive(arPfad->str);
